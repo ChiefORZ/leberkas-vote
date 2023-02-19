e5b566b7161c36f2d1796e945d81083a00445926
ChiefORZ <seb.schaffernak@gmail.com>
2023-02-19 23:12:46 +0700

    chore: remove debug statements

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,14 @@
 import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 import { withAuth } from 'next-auth/middleware';
+
 const secret = process.env.NEXTAUTH_SECRET;
 
 const protectedPages = ['/results'];
 
 export default withAuth(
   async function middleware(req) {
-    console.log('secret ', secret);
     const token = await getToken({ req, secret: secret });
-    // log req cookies
-    console.log(
-      'req.cookies ',
-      req.cookies.get('next-auth.session-token'),
-      req?.cookies.get('__Secure-next-auth.session-token')
-    );
-    // log req authorization header
-    console.log(
-      'req.headers.authorization ',
-      req.headers.get('authorization'),
-      req?.headers?.['authorization'],
-      req?.headers?.['Authorization']
-    );
     const isAuth = !!token;
     const isAuthPage = req.nextUrl.pathname.startsWith('/login');
 
