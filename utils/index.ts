1b3d796cd2c8cd91e10146d97014950b5102cca0
ChiefORZ <seb.schaffernak@gmail.com>
2023-06-03 19:13:23 +0700

    feat: add awesome classNames fn

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,6 @@
-export function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function classNames(...classes: ClassValue[]) {
+  return twMerge(clsx(classes));
 }
