dfc1dff6167d868e51a34c5168d2348dde5bbd22
ChiefORZ <seb.schaffernak@gmail.com>
2024-02-12 20:09:59 +0700

    chore: Ignore eslint during builds

--- next.config.js
+++ next.config.js
@@ -1,39 +1,44 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      'lh3.googleusercontent.com',
-      'googleusercontent.com',
-      'basil-images-dev.s3.eu-west-1.amazonaws.com',
-    ],
-    unoptimized: true,
-  },
-  experimental: {
-    appDir: true,
-    // serverComponentsExternalPackages: ["@prisma/client"],
-  },
-  reactStrictMode: true,
-  rewrites: async () => [
-    {
-      source: '/bee.js',
-      destination: 'https://cdn.splitbee.io/sb.js',
-    },
-    {
-      source: '/_hive/:slug',
-      destination: 'https://hive.splitbee.io/:slug',
-    },
-  ],
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  webpack: (config, { isServer, nextRuntime }) => {
-    // Avoid AWS SDK Node.js require issue
-    if (isServer && nextRuntime === 'nodejs')
-      config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /^aws-crt$/ }));
-    return config;
-  },
+	images: {
+		domains: [
+			"lh3.googleusercontent.com",
+			"googleusercontent.com",
+			"basil-images-dev.s3.eu-west-1.amazonaws.com",
+		],
+		unoptimized: true,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	experimental: {
+		appDir: true,
+		// serverComponentsExternalPackages: ["@prisma/client"],
+	},
+	reactStrictMode: true,
+	rewrites: async () => [
+		{
+			source: "/bee.js",
+			destination: "https://cdn.splitbee.io/sb.js",
+		},
+		{
+			source: "/_hive/:slug",
+			destination: "https://hive.splitbee.io/:slug",
+		},
+	],
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	webpack: (config, { isServer, nextRuntime }) => {
+		// Avoid AWS SDK Node.js require issue
+		if (isServer && nextRuntime === "nodejs")
+			config.plugins.push(
+				new webpack.IgnorePlugin({ resourceRegExp: /^aws-crt$/ }),
+			);
+		return config;
+	},
 };
 
 module.exports = nextConfig;
